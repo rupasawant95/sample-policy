a5e7e3de9fbbda5e5e763810eb983d0755032c54
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-31 15:17:08 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -6,7 +6,14 @@ resource "aws_security_group" "example" {
   name        = "example-sg"
   description = "Example security group"
 
-  // Define inbound rules
+  # Define inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -14,17 +21,11 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // Define outbound rules
+  # Define outbound rules
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
