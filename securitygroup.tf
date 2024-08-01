45d578ce82347dd108072cce2d5a65db0e7328e0
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-01 13:48:37 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,9 +1,20 @@
 provider "aws" {
+    region = "us-west-2"
+}
+resource "aws_instance" "terr-inc" {
+    ami         = "ami-074be47313f84fa38"
+    instance_type = "t2-micro"
+    security_groups = [aws_security_group_rule.terr-inc.name]
+    tags = {
+    Name = "Sentinel-check"
+    }
+}
+provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "example" {
-  name        = "aws_security_group_rule"
+resource "aws_security_group_rule" "example" {
+  name        = "terra_security_group"
   description = "Example security group"
 
   # Define inbound rules
